2c1bcadfe938b1fd71347e4c9d81df0a8f74309e
naruto-f <476971219@qq.com>
2022-08-14 00:55:32 +0800

    程序大致定型，还有重定向和多指令并行功能未实现

--- processes-shell/shell.c
+++ processes-shell/shell.c
@@ -4,12 +4,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
 
+#define MAX_ARG_NUM 5
+#define MAX_PID_NUM 20
 
 struct ShellPathNode
 {
     const char* shellPath;
-    struct ShellPathNode* next = NULL;
+    struct ShellPathNode* next;
 };
 
 /* 管理shellpath的链表的头节点 */
@@ -33,13 +39,19 @@ void initShellPath()
 
 void freeShellPathList()
 {
-    struct ShellPathNode *curNode = pathHead;
+    struct ShellPathNode *curNode = pathHead->next;
     while(curNode)
     {
         struct ShellPathNode *nextNode = curNode->next;
         free(curNode);
         curNode = nextNode;
     }
+    pathHead->next = NULL;
+}
+
+int isShellPathEmpty()
+{
+    return !(pathHead->next == NULL);
 }
 
 
@@ -53,12 +65,155 @@ void printErrorMassageToStderr()
 void freeDynamicMemory()
 {
     freeShellPathList();
+    free(pathHead);
 }
 
+void exitForErrorWithoutMemoryLeak()
+{
+    freeDynamicMemory();
+    printErrorMassageToStderr();
+    exit(1);
+}
+
+int checkvalidityOfNonBuiltInCommand(const char* command)
+{
+    struct ShellPathNode* workNode = pathHead->next;
+    while(workNode)
+    {
+        struct ShellPathNode* nextNode = workNode->next;
+
+        char* absolutePath = (char*)malloc(sizeof(strlen(workNode->shellPath) + strlen(command) + 1));
+        sprintf(absolutePath, "%s%s%s", workNode->shellPath, "/", command);
+        if(access(absolutePath, X_OK) == 0)
+        {
+            return 0;
+        }
+        free(absolutePath);
+
+        workNode = nextNode;
+    }
+
+    return -1;
+}
+
+int checkIsCommandBuiltIn(const char* command)
+{
+    return !strcmp(command, "exit") && !strcmp(command, "exit") && !strcmp(command, "exit");
+}
+
+void processExit()
+{
+    freeDynamicMemory();
+    exit(0);
+}
+
+void processPath(int argc, char* argv[])
+{
+    freeShellPathList();
+
+    for(int i = 0; i < argc; ++i)
+    {
+        insertNewShellPath(argv[i]);
+    }
+}
+
+int processCd(const char* dirName)
+{
+    if(chdir(dirName) == -1)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+void runBuiltInCommand(const char* command, int argc, char* argv[])
+{
+    if(!strcmp(command, "exit"))
+    {
+        processExit();
+    }
+    else if(!strcmp(command, "cd"))
+    {
+        if(argc != 1 || processCd(argv[0]) == -1)
+        {
+            printErrorMassageToStderr();
+        }
+    }
+    else
+    {
+        processPath(argc, argv);
+    }
+}
 
 void processInstruction(char* instruction)
 {
+    char* argv[MAX_ARG_NUM] = { NULL };
+    char* command = NULL;
+    char* temp = NULL;
+    int count = 0;
+    int pidNum = 0;
 
+    while((temp = strsep(&instruction, " ")) != NULL)
+    {
+        if(count == 0)
+        {
+            command = temp;
+        }
+        else
+        {
+            argv[count - 1] = temp;
+        }
+
+        ++count;
+    }
+
+    if(checkIsCommandBuiltIn(command) == 0)
+    {
+        runBuiltInCommand(command, 0, argv);
+    }
+    else if(checkvalidityOfNonBuiltInCommand(command) == -1 || !isShellPathEmpty())
+    {
+        /* 如果非内建命令不合法则直接退出shell */
+        exitForErrorWithoutMemoryLeak();
+    }
+
+    pid_t pidSet[MAX_PID_NUM] = { 0 };
+    unsigned int pidSetPos = 0;
+    for(int i = 0; i < pidNum; ++i)
+    {
+        pid_t pid = fork();
+        if(pid == -1)
+        {
+            exitForErrorWithoutMemoryLeak();
+        }
+
+        if(pid == 0)
+        {
+            if(execv(command, argv) == -1)
+            {
+                exitForErrorWithoutMemoryLeak();
+            }
+        }
+        else
+        {
+            pidSet[pidSetPos++] = pid;
+        }
+    }
+
+    int childErrorFlag = 0;
+    for(int i = 0; i < pidSetPos; ++i)
+    {
+        if(waitpid(pidSet[i], NULL, WUNTRACED) != pidSet[i])
+        {
+            childErrorFlag = 1;
+        }
+    }
+
+    if(childErrorFlag)
+    {
+        exitForErrorWithoutMemoryLeak();
+    }
 }
 
 void shellRun(FILE *fp, const char* prompt)
@@ -88,8 +243,7 @@ void startBatchMode(const char* batchFileName)
     FILE *fp = fopen(batchFileName, "r");
     if(fp == NULL)
     {
-        printErrorMassageToStderr();
-        exit(1);
+        exitForErrorWithoutMemoryLeak();
     }
 
     shellRun(fp, "");
@@ -114,8 +268,6 @@ int main(int argc, char* argv[])
     {
         startBatchMode(argv[1]);
     }
-
-    freeDynamicMemory();
 
     return 0;
 }
